28f2dd8d56e6ab9501d6b3c9679b223616859cd5
3052 <spen+3052@protonmail.com>
2024-11-16 09:24:58 -0600

    +type cdm struct{}

--- blog/playReady/cdm.go
+++ blog/playReady/cdm.go
@@ -1,31 +1,11 @@
 package playReady
 
-def get_license_challenge(self, session_id: bytes, content_header: str) -> str:
-  session = self.__sessions.get(session_id)
-  if not session:
-      raise InvalidSession(f"Session identifier {session_id!r} is invalid.")
+import (
+)
 
-  session.signing_key = self.signing_key
-  session.encryption_key = self.encryption_key
+type cdm struct{}
 
-  la_content = self._build_digest_content(
-      content_header=content_header,
-      nonce=base64.b64encode(get_random_bytes(16)).decode(),
-      wmrm_cipher=base64.b64encode(self._get_key_data(session)).decode(),
-      cert_cipher=base64.b64encode(self._get_cipher_data(session)).decode()
-  )
-
-  la_hash_obj = SHA256.new()
-  la_hash_obj.update(la_content.encode())
-  la_hash = la_hash_obj.digest()
-
-  signed_info = self._build_signed_info(base64.b64encode(la_hash).decode())
-  signed_info_digest = SHA256.new(signed_info.encode())
-
-  signer = DSS.new(session.signing_key.key, 'fips-186-3')
-  signature = signer.sign(signed_info_digest)
-
-  # haven't found a better way to do this. xmltodict.unparse doesn't work
+func (cdm) license_challenge(session_id []byte, content_header string) string {
   main_body = (
       '<?xml version="1.0" encoding="utf-8"?>'
       '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">'
@@ -51,61 +31,122 @@ def get_license_challenge(self, session_id: bytes, content_header: str) -> str:
           '</soap:Body>'
       '</soap:Envelope>'
   )
-
   return main_body
+}
 
-def _decrypt_ecc256_key(self, session: Session, encrypted_key: bytes) -> bytes:
-  point1 = Point(
-      x=int.from_bytes(encrypted_key[:32], 'big'),
-      y=int.from_bytes(encrypted_key[32:64], 'big'),
-      curve=self.curve
-  )
-  point2 = Point(
-      x=int.from_bytes(encrypted_key[64:96], 'big'),
-      y=int.from_bytes(encrypted_key[96:128], 'big'),
-      curve=self.curve
-  )
-
-  decrypted = self.elgamal.decrypt((point1, point2), int(session.encryption_key.key.d))
-  return self.elgamal.to_bytes(decrypted.x)[16:32]
-
-@staticmethod
-def _verify_ecc_key(session: Session, licence: XMRLicense) -> bool:
-  ecc_keys = list(licence.get_object(42))
-  if not ecc_keys:
-      raise InvalidLicense("No ECC public key in license")
-
-  return ecc_keys[0].key == session.encryption_key.public_bytes()
-
-def parse_license(self, session_id: bytes, licence: str) -> None:
-  session = self.__sessions.get(session_id)
-  if not session:
-      raise InvalidSession(f"Session identifier {session_id!r} is invalid.")
-
-  try:
-      root = ET.fromstring(licence)
-      license_elements = root.findall(".//{http://schemas.microsoft.com/DRM/2007/03/protocols}License")
-      for license_element in license_elements:
-          parsed_licence = XMRLicense.loads(license_element.text)
-
-          if not self._verify_ecc_key(session, parsed_licence):
-              raise InvalidLicense("Public encryption key does not match")
-
-          for key in parsed_licence.get_content_keys():
-              if Key.CipherType(key.cipher_type) == Key.CipherType.ECC256:
-                  session.keys.append(Key(
-                      key_id=UUID(bytes_le=key.key_id),
-                      key_type=key.key_type,
-                      cipher_type=key.cipher_type,
-                      key_length=key.key_length,
-                      key=self._decrypt_ecc256_key(session, key.encrypted_key)
-                  ))
-  except Exception as e:
-      raise Exception(f"Unable to parse license, {e}")
-
-def get_keys(self, session_id: bytes) -> List[Key]:
-  session = self.__sessions.get(session_id)
-  if not session:
-      raise InvalidSession(f"Session identifier {session_id!r} is invalid.")
-
-  return session.keys
+type Envelope struct {
+   XMLName xml.Name `xml:"Envelope"`
+   Text    string   `xml:",chardata"`
+   Soap    string   `xml:"soap,attr"`
+   Body    struct {
+      Text           string `xml:",chardata"`
+      AcquireLicense struct {
+         Text      string `xml:",chardata"`
+         Xmlns     string `xml:"xmlns,attr"`
+         Challenge struct {
+            Text      string `xml:",chardata"`
+            Challenge struct {
+               Text  string `xml:",chardata"`
+               Xmlns string `xml:"xmlns,attr"`
+               LA    struct {
+                  Text          string `xml:",chardata"`
+                  Xmlns         string `xml:"xmlns,attr"`
+                  ID            string `xml:"Id,attr"`
+                  Space         string `xml:"space,attr"`
+                  Version       string `xml:"Version"`
+                  ContentHeader struct {
+                     Text      string `xml:",chardata"`
+                     WRMHEADER struct {
+                        Text    string `xml:",chardata"`
+                        Xmlns   string `xml:"xmlns,attr"`
+                        Version string `xml:"version,attr"`
+                        DATA    struct {
+                           Text        string `xml:",chardata"`
+                           PROTECTINFO struct {
+                              Text   string `xml:",chardata"`
+                              KEYLEN string `xml:"KEYLEN"`
+                              ALGID  string `xml:"ALGID"`
+                           } `xml:"PROTECTINFO"`
+                           KID string `xml:"KID"`
+                        } `xml:"DATA"`
+                     } `xml:"WRMHEADER"`
+                  } `xml:"ContentHeader"`
+                  CLIENTINFO struct {
+                     Text          string `xml:",chardata"`
+                     CLIENTVERSION string `xml:"CLIENTVERSION"`
+                  } `xml:"CLIENTINFO"`
+                  RevocationLists struct {
+                     Text        string `xml:",chardata"`
+                     RevListInfo []struct {
+                        Text    string `xml:",chardata"`
+                        ListID  string `xml:"ListID"`
+                        Version string `xml:"Version"`
+                     } `xml:"RevListInfo"`
+                  } `xml:"RevocationLists"`
+                  LicenseNonce  string `xml:"LicenseNonce"`
+                  ClientTime    string `xml:"ClientTime"`
+                  EncryptedData struct {
+                     Text             string `xml:",chardata"`
+                     Xmlns            string `xml:"xmlns,attr"`
+                     Type             string `xml:"Type,attr"`
+                     EncryptionMethod struct {
+                        Text      string `xml:",chardata"`
+                        Algorithm string `xml:"Algorithm,attr"`
+                     } `xml:"EncryptionMethod"`
+                     KeyInfo struct {
+                        Text         string `xml:",chardata"`
+                        Xmlns        string `xml:"xmlns,attr"`
+                        EncryptedKey struct {
+                           Text             string `xml:",chardata"`
+                           Xmlns            string `xml:"xmlns,attr"`
+                           EncryptionMethod struct {
+                              Text      string `xml:",chardata"`
+                              Algorithm string `xml:"Algorithm,attr"`
+                           } `xml:"EncryptionMethod"`
+                           KeyInfo struct {
+                              Text    string `xml:",chardata"`
+                              Xmlns   string `xml:"xmlns,attr"`
+                              KeyName string `xml:"KeyName"`
+                           } `xml:"KeyInfo"`
+                           CipherData struct {
+                              Text        string `xml:",chardata"`
+                              CipherValue string `xml:"CipherValue"`
+                           } `xml:"CipherData"`
+                        } `xml:"EncryptedKey"`
+                     } `xml:"KeyInfo"`
+                     CipherData struct {
+                        Text        string `xml:",chardata"`
+                        CipherValue string `xml:"CipherValue"`
+                     } `xml:"CipherData"`
+                  } `xml:"EncryptedData"`
+               } `xml:"LA"`
+               Signature struct {
+                  Text       string `xml:",chardata"`
+                  SignedInfo struct {
+                     Text                   string `xml:",chardata"`
+                     Xmlns                  string `xml:"xmlns,attr"`
+                     CanonicalizationMethod struct {
+                        Text      string `xml:",chardata"`
+                        Algorithm string `xml:"Algorithm,attr"`
+                     } `xml:"CanonicalizationMethod"`
+                     SignatureMethod struct {
+                        Text      string `xml:",chardata"`
+                        Algorithm string `xml:"Algorithm,attr"`
+                     } `xml:"SignatureMethod"`
+                     Reference struct {
+                        Text         string `xml:",chardata"`
+                        URI          string `xml:"URI,attr"`
+                        DigestMethod struct {
+                           Text      string `xml:",chardata"`
+                           Algorithm string `xml:"Algorithm,attr"`
+                        } `xml:"DigestMethod"`
+                        DigestValue string `xml:"DigestValue"`
+                     } `xml:"Reference"`
+                  } `xml:"SignedInfo"`
+                  SignatureValue string `xml:"SignatureValue"`
+               } `xml:"Signature"`
+            } `xml:"Challenge"`
+         } `xml:"challenge"`
+      } `xml:"AcquireLicense"`
+   } `xml:"Body"`
+} 
