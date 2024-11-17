ba4cff2d4bd637978e0991ebc0c0758703def052
3052 <spen+3052@protonmail.com>
2024-11-17 09:25:25 -0600

    renamed:    blog/2024-11-11/mspr_toolkit.md -> blog/2024-11-17/mspr_toolkit.md

--- playReady/cdm.go
+++ playReady/cdm.go
@@ -2,9 +2,97 @@ package playReady
 
 import (
    "bytes"
+   "crypto/ecdh"
    "net/http"
    "playReady/etree"
 )
+
+func digest_value() (string, error) {
+   key, err := os.ReadFile("zgpriv.dat")
+   if err != nil {
+      return "", err
+   }
+   encryption_key, err := ecdh.P256().NewPrivateKey(key)
+   if err != nil {
+      return "", err
+   }
+   signing_key, err := ecdh.P256().NewPrivateKey(key)
+   if err != nil {
+      return "", err
+   }
+   group_key, err := ecdh.P256().NewPrivateKey(key)
+   if err != nil {
+      return "", err
+   }
+   BCert = Struct(
+      "signature" / Const(b"CERT"),
+      "version" / Int32ub,
+      "total_length" / Int32ub,
+      "certificate_length" / Int32ub,
+      "attributes" / GreedyRange(Attribute)
+   )
+   BCertChain = Struct(
+      "signature" / Const(b"CHAI"),
+      "version" / Int32ub,
+      "total_length" / Int32ub,
+      "flags" / Int32ub,
+      "certificate_count" / Int32ub,
+      "certificates" / GreedyRange(BCert)
+   )
+   cert_chain = _BCertStructs.BCertChain
+   return cls(
+      parsed_bcert_chain=cert_chain.parse(data),
+      bcert_chain_obj=cert_chain
+   )
+   with Path(path).open(mode="rb") as f:
+      return cls.loads(f.read())
+   certificate_chain = CertificateChain.load(group_certificate)
+   new_certificate = Certificate.new_leaf_cert(
+      group_key=group_key,
+      parent=certificate_chain
+      security_level=certificate_chain.get_security_level(),
+      client_id=get_random_bytes(16),
+      cert_id=get_random_bytes(16),
+      signing_key=signing_key,
+      encryption_key=encryption_key,
+   )
+   certificate_chain.prepend(new_certificate)
+   device = Device(
+      group_certificate=certificate_chain.dumps(),
+      encryption_key=encryption_key.dumps(),
+      signing_key=signing_key.dumps()
+   )
+   cdm = Cdm.from_device(device)
+   session = self.__sessions.get(session_id)
+   session.encryption_key = self.encryption_key
+   session.signing_key = self.signing_key
+   la_content = self._build_digest_content(
+      wmrm_cipher=base64.b64encode(self._get_key_data(session)).decode(),
+      cert_cipher=base64.b64encode(self._get_cipher_data(session)).decode()
+      content_header=content_header,
+      nonce=base64.b64encode(get_random_bytes(16)).decode(),
+   )
+   la_hash_obj = SHA256.new()
+   la_hash_obj.update(la_content.encode())
+   la_hash = la_hash_obj.digest()
+   return base64.b64encode(la_hash).decode()
+}
+
+func license() (*http.Response, error) {
+   data := &bytes.Buffer{}
+   _, err := license_challenge().WriteTo(data)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://drm-playready-licensing.axtest.net/AcquireLicense", data,
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("x-axdrm-message", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjMzNjRlYjUtNTFmNi00YWUzLThjOTgtMzNjZWQ1ZTMxYzc4IiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsImtleXMiOlt7ImlkIjoiMDg3Mjc4NmUtZjllNy00NjVmLWEzYTItNGU1YjBlZjhmYTQ1IiwiZW5jcnlwdGVkX2tleSI6IlB3NitlRVlOY3ZqWWJmc2gzWDNmbWc9PSJ9LHsiaWQiOiJjMTRmMDcwOS1mMmI5LTQ0MjctOTE2Yi02MWI1MjU4NjUwNmEiLCJlbmNyeXB0ZWRfa2V5IjoiLzErZk5paDM4bXFSdjR5Y1l6bnQvdz09In0seyJpZCI6IjhiMDI5ZTUxLWQ1NmEtNDRiZC05MTBmLWQ0YjVmZDkwZmJhMiIsImVuY3J5cHRlZF9rZXkiOiJrcTBKdVpFanBGTjhzYVRtdDU2ME9nPT0ifSx7ImlkIjoiMmQ2ZTkzODctNjBjYS00MTQ1LWFlYzItYzQwODM3YjRiMDI2IiwiZW5jcnlwdGVkX2tleSI6IlRjUlFlQld4RW9IT0tIcmFkNFNlVlE9PSJ9LHsiaWQiOiJkZTAyZjA3Zi1hMDk4LTRlZTAtYjU1Ni05MDdjMGQxN2ZiYmMiLCJlbmNyeXB0ZWRfa2V5IjoicG9lbmNTN0dnbWVHRmVvSjZQRUFUUT09In0seyJpZCI6IjkxNGU2OWY0LTBhYjMtNDUzNC05ZTlmLTk4NTM2MTVlMjZmNiIsImVuY3J5cHRlZF9rZXkiOiJlaUkvTXNsbHJRNHdDbFJUL0xObUNBPT0ifSx7ImlkIjoiZGE0NDQ1YzItZGI1ZS00OGVmLWIwOTYtM2VmMzQ3YjE2YzdmIiwiZW5jcnlwdGVkX2tleSI6IjJ3K3pkdnFycERWM3hSMGJKeTR1Z3c9PSJ9LHsiaWQiOiIyOWYwNWU4Zi1hMWFlLTQ2ZTQtODBlOS0yMmRjZDQ0Y2Q3YTEiLCJlbmNyeXB0ZWRfa2V5IjoiL3hsU0hweHdxdTNnby9nbHBtU2dhUT09In0seyJpZCI6IjY5ZmU3MDc3LWRhZGQtNGI1NS05NmNkLWMzZWRiMzk5MTg1MyIsImVuY3J5cHRlZF9rZXkiOiJ6dTZpdXpOMnBzaTBaU3hRaUFUa1JRPT0ifV19fQ.BXr93Et1krYMVs-CUnf7F3ywJWFRtxYdkR7Qn4w3-to")
+   return http.DefaultClient.Do(req)
+}
 
 func license_challenge() *etree.Document {
    doc := etree.NewDocument()
@@ -134,6 +222,7 @@ func license_challenge() *etree.Document {
                            e.Create("DigestMethod", func(e *etree.Element) {
                               e.CreateAttr("Algorithm", "http://schemas.microsoft.com/DRM/2007/03/protocols#sha256")
                            })
+/////////////////////////////////////////////////////////////////////////////////
                            e.Create("DigestValue", func(e *etree.Element) {
                               e.CreateText("3ZuoaJqKnCfQkMWOvfjDpmnO3gMwFwZf8jOHkcFRvDE=")
                            })
@@ -142,6 +231,7 @@ func license_challenge() *etree.Document {
                      e.Create("SignatureValue", func(e *etree.Element) {
                         e.CreateText("5Z01ixAuF9AD3PYXiLft5U+r/I19ivCYWo00snefYWktzNbZ9OpSauq3kW04xHgtPeomb3TWN49XtmV1mWMwhg==")
                      })
+/////////////////////////////////////////////////////////////////////////////////
                   })
                })
             })
@@ -149,20 +239,4 @@ func license_challenge() *etree.Document {
       })
    })
    return doc
-}
-
-func license() (*http.Response, error) {
-   data := &bytes.Buffer{}
-   _, err := license_challenge().WriteTo(data)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://drm-playready-licensing.axtest.net/AcquireLicense", data,
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("x-axdrm-message", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiYjMzNjRlYjUtNTFmNi00YWUzLThjOTgtMzNjZWQ1ZTMxYzc4IiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsImtleXMiOlt7ImlkIjoiMDg3Mjc4NmUtZjllNy00NjVmLWEzYTItNGU1YjBlZjhmYTQ1IiwiZW5jcnlwdGVkX2tleSI6IlB3NitlRVlOY3ZqWWJmc2gzWDNmbWc9PSJ9LHsiaWQiOiJjMTRmMDcwOS1mMmI5LTQ0MjctOTE2Yi02MWI1MjU4NjUwNmEiLCJlbmNyeXB0ZWRfa2V5IjoiLzErZk5paDM4bXFSdjR5Y1l6bnQvdz09In0seyJpZCI6IjhiMDI5ZTUxLWQ1NmEtNDRiZC05MTBmLWQ0YjVmZDkwZmJhMiIsImVuY3J5cHRlZF9rZXkiOiJrcTBKdVpFanBGTjhzYVRtdDU2ME9nPT0ifSx7ImlkIjoiMmQ2ZTkzODctNjBjYS00MTQ1LWFlYzItYzQwODM3YjRiMDI2IiwiZW5jcnlwdGVkX2tleSI6IlRjUlFlQld4RW9IT0tIcmFkNFNlVlE9PSJ9LHsiaWQiOiJkZTAyZjA3Zi1hMDk4LTRlZTAtYjU1Ni05MDdjMGQxN2ZiYmMiLCJlbmNyeXB0ZWRfa2V5IjoicG9lbmNTN0dnbWVHRmVvSjZQRUFUUT09In0seyJpZCI6IjkxNGU2OWY0LTBhYjMtNDUzNC05ZTlmLTk4NTM2MTVlMjZmNiIsImVuY3J5cHRlZF9rZXkiOiJlaUkvTXNsbHJRNHdDbFJUL0xObUNBPT0ifSx7ImlkIjoiZGE0NDQ1YzItZGI1ZS00OGVmLWIwOTYtM2VmMzQ3YjE2YzdmIiwiZW5jcnlwdGVkX2tleSI6IjJ3K3pkdnFycERWM3hSMGJKeTR1Z3c9PSJ9LHsiaWQiOiIyOWYwNWU4Zi1hMWFlLTQ2ZTQtODBlOS0yMmRjZDQ0Y2Q3YTEiLCJlbmNyeXB0ZWRfa2V5IjoiL3hsU0hweHdxdTNnby9nbHBtU2dhUT09In0seyJpZCI6IjY5ZmU3MDc3LWRhZGQtNGI1NS05NmNkLWMzZWRiMzk5MTg1MyIsImVuY3J5cHRlZF9rZXkiOiJ6dTZpdXpOMnBzaTBaU3hRaUFUa1JRPT0ifV19fQ.BXr93Et1krYMVs-CUnf7F3ywJWFRtxYdkR7Qn4w3-to")
-   return http.DefaultClient.Do(req)
 }
--- playReady/etree/etree_test.go
+++ playReady/etree/etree_test.go
@@ -7,12 +7,21 @@ import (
 
 func TestElement(t *testing.T) {
    doc := NewDocument()
-   doc.Element().Create("People", func(e *Element) {
-      e.Create("Person", func(e *Element) {
-         e.CreateAttr("name", "Jon")
-      })
-      e.Create("Person", func(e *Element) {
-         e.CreateAttr("name", "Sally")
+   doc.Element().Create("WRMHEADER", func(e *Element) {
+      e.CreateAttr("xmlns", "http://schemas.microsoft.com/DRM/2007/03/PlayReadyHeader")
+      e.CreateAttr("version", "4.0.0.0")
+      e.Create("DATA", func(e *Element) {
+         e.Create("PROTECTINFO", func(e *Element) {
+            e.Create("KEYLEN", func(e *Element) {
+               e.CreateText("16")
+            })
+            e.Create("ALGID", func(e *Element) {
+               e.CreateText("AESCTR")
+            })
+         })
+         e.Create("KID", func(e *Element) {
+            e.CreateText("UZ4Ci2rVvUSRD9S1/ZD7og==")
+         })
       })
    })
    doc.Indent(1)
