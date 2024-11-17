44c32ae4a7f56f4d16b409b81b56869aa0a83f10
3052 <spen+3052@protonmail.com>
2024-11-17 00:01:48 -0600

    +func license_challenge() *etree.Document {

--- playReady/cdm_test.go
+++ playReady/cdm_test.go
@@ -5,8 +5,17 @@ import (
    "testing"
 )
 
-func TestCdm(t *testing.T) {
-   doc := get_license_challenge()
+func TestLicense(t *testing.T) {
+   resp, err := license()
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer resp.Body.Close()
+   resp.Write(os.Stdout)
+}
+
+func TestChallenge(t *testing.T) {
+   doc := license_challenge()
    doc.Indent(1)
    doc.WriteTo(os.Stdout)
 }
