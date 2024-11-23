3ee367e757c1649b1fa86c3adb48606a908f2de4
3052 <spen+3052@protonmail.com>
2024-11-23 13:12:02 -0600

    +   data, err := os.ReadFile("../secrets/g1")

--- playReady/bcert_test.go
+++ playReady/bcert_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func TestBcert(t *testing.T) {
-   buf, err := os.ReadFile("../secrets/g1")
+   data, err := os.ReadFile("../secrets/g1")
    if err != nil {
       t.Fatal(err)
    }
    var bcert bcert_chain
    slog.SetLogLoggerLevel(slog.LevelDebug)
-   err = bcert.read(buf)
+   err = bcert.read(data)
    if err != nil {
       t.Fatal(err)
    }
