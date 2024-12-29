d999ca356e74826ef4553fe3a463a73628e405af
3052 <spen+3052@protonmail.com>
2024-12-29 06:24:16 -0600

    +   eAlign uint32

--- blog/2024-12-28/prkb.go
+++ blog/2024-12-28/prkb.go
@@ -5,20 +5,10 @@ import (
    "strconv"
 )
 
-func (p *prkb) String() string {
-   b := []byte("qwFormatIdentifier = ")
-   b = strconv.AppendQuote(b, string(p.qwFormatIdentifier[:]))
-   b = append(b, "\ndwFormatVersion = "...)
-   b = strconv.AppendUint(b, uint64(p.dwFormatVersion), 10)
-   b = append(b, "\nc = "...)
-   b = strconv.AppendQuote(b, string(p.c))
-   return string(b)
-}
-
 type prkb struct {
    qwFormatIdentifier [4]byte
    dwFormatVersion uint32
-   c []byte
+   eAlign uint32
 }
 
 func (p *prkb) decode(data []byte) {
@@ -26,5 +16,15 @@ func (p *prkb) decode(data []byte) {
    data = data[4:]
    p.dwFormatVersion = binary.BigEndian.Uint32(data)
    data = data[4:]
-   p.c = data[:p.dwFormatVersion]
+   p.eAlign = binary.BigEndian.Uint32(data)
+}
+
+func (p *prkb) String() string {
+   b := []byte("qwFormatIdentifier = ")
+   b = strconv.AppendQuote(b, string(p.qwFormatIdentifier[:]))
+   b = append(b, "\ndwFormatVersion = "...)
+   b = strconv.AppendUint(b, uint64(p.dwFormatVersion), 10)
+   b = append(b, "\neAlign = "...)
+   b = strconv.AppendUint(b, uint64(p.eAlign), 10)
+   return string(b)
 }
--- blog/2024-12-28/prkb_test.go
+++ blog/2024-12-28/prkb_test.go
@@ -2,6 +2,7 @@ package playReady
 
 import (
    "bytes"
+   "encoding/binary"
    "fmt"
    "os"
    "testing"
@@ -12,8 +13,24 @@ func TestPrkb(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   i := bytes.Index(data, []byte("PRKB"))
-   var play prkb
-   play.decode(data[i:])
-   fmt.Println(&play)
+   i := bytes.Index(data, []byte("CHAI"))
+   data = data[i:]
+   i = bytes.Index(data, bytes.Repeat([]byte{0}, 19))
+   data = data[:i]
+   fmt.Printf("%q\n", data)
+   size := uint32(len(data))
+   fmt.Println(size)
+   fmt.Printf("%x\n", binary.BigEndian.AppendUint32(nil, size))
+   fmt.Printf("%q\n", binary.BigEndian.AppendUint32(nil, size))
 }
+
+/*
+from what I can tell the size of the g1 is 1856 - which would be
+```
+00 00 07 40
+```
+or
+```
+\x00\x00\a@
+```
+*/
